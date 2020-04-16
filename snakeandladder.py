642016d79634b0adfa74e8c226d81c3ddf242c8a
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-04-16 14:06:53 +0530

    [sachin]Add.added rollDice function to get the next position

--- snakeandladder.py
+++ snakeandladder.py
@@ -6,6 +6,11 @@ class Player:
 		self.playerPos = 1
 		self.playerNum = inPlayerNum
 
+
+# Function to return the Integer value of a 6 side dice roll
+def rollDice():
+    return random.randint(1,6)
+	
 # Program entrance
 if __name__ == '__main__':
 	global winner
