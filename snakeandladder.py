4fc197fcc66c6af284a46d02865c5d5dbfded906
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-04-16 17:11:02 +0530

    [sachin]Add.added dictionary for sanke and ladder positions and few methods

--- snakeandladder.py
+++ snakeandladder.py
@@ -1,18 +1,51 @@
 import random
 
+# Snakes and Ladders dictionary
+SnLadDict = {
+		  7: 18,
+          16: 3,
+          22: 15,
+          26: 21,
+          35: 44,
+          39: 35,
+          46: 62,
+          67: 58,
+          70: 58,
+          78: 69,
+          85: 86,
+          86: 81,
+          88: 30,
+		  92: 55,
+		  99: 2
+        }
+
+
 # Player class
-class Player:
+class SnakeLadder:
 	def __init__(self, inPlayerNum):
-		self.playerPos = 1
+		self.playerPos = 0
 		self.playerNum = inPlayerNum
 
+	def updatePosition(self, inPos):
+		self.playerPos = inPos
 
-# Function to return the Integer value of a 6 side dice roll
-def rollDice():
-    return random.randint(1,6)
-	
+	def getPosition(self):
+		return self.playerPos
+
+	def getPlayerNum(self):
+		return self.playerNum
+
 # Program entrance
 if __name__ == '__main__':
 	global winner
 	winner = False
 	numPlayers = int(input('Enter number of players: '))
+
+	# If only one player is playing ,then second player is computer
+	if numPlayers == 1 :
+		numPlayers+=1
+	else:
+		numPlayers
+
+	for i in range(0,numPlayers):
+		playerList.append(SnakeLadder(i + 1))
